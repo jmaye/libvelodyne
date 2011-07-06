b86d8b5bdb3ee5b1d51b39cf2bebffa968b0a9e7
Jerome Maye <jerome.maye@mavt.ethz.ch>
2011-07-06 12:38:49 +0000

    small change

--- src/bin/liveView.cpp
+++ src/bin/liveView.cpp
@@ -27,7 +27,7 @@
 
 int main(int argc, char** argv) {
   if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << "<calibrationFile>"
+    std::cerr << "Usage: " << argv[0] << " <calibrationFile>"
       << std::endl;
     return -1;
   }
